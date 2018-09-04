1aaa97a3e76ba1ba9666b81b654b0134161b728c
yfujita0101 <yFujita0101@gmail.com>
2018-09-04 22:50:29 +0900

    add update and delete function'

--- client/src/components/Posts.vue
+++ client/src/components/Posts.vue
@@ -2,21 +2,31 @@
   <!-- 表示画面 -->
   <div class="container">
     <div class="row">
-      <div class="col-sm-10">
+      <div class="col-sm-12">
         <h1>Posts</h1>
         <hr><br><br>
         <alert :message="message" v-if="showMessage"></alert>
         <button type="button" class="btn btn-success btn-sm" v-b-modal.post-modal>Add Posts</button>
         <br><br>
-        <div class="card-deck">
+        <div class="card-columns">
           <div v-for="(post, index) in posts" :key="index"
-                class="card border-primay mb-3" style="width: 18rem;">
+                class="card border-primay mb-6" style="width: 18rem;">
             <div class="card-header">{{ post.title }}</div>
             <div class="card-body text-primary">
               <p class="card-text">{{ post.body }}</p>
               <div class="text-right">
-                <a href="#" class="btn btn-primary">update</a>
-                <a href="#" class="btn btn-warning">delete</a>
+                <button type="button"
+                        class="btn btn-primary btn-sm"
+                        v-b-modal.post-update-modal
+                        @click="editPost(post)">
+                  update
+                </button>
+                <button
+                    type="button"
+                    class="btn btn-warning btn-sm"
+                    @click="onDeletePost(post)">
+                    delete
+                  </button>
               </div>
               <p class="card-text text-right">
                 <small class="text-muted">
@@ -60,8 +70,39 @@
         <b-button type="reset" variant="danger">Reset</b-button>
       </b-form>
     </b-modal>
-  </div>
 
+    <!-- edit post modal -->
+    <b-modal ref="editPostModal"
+              id="post-update-modal"
+              title="Update"
+              hide-footer>
+      <b-form @submit="onSubmitUpdate" @reset="onResetUpdate" class="w-100">
+        <b-form-group id="form-title-edit-group"
+                      label="Title:"
+                      label-for="form-title-edit-input">
+          <b-form-input id="form-title-edit-input"
+                        type="text"
+                        v-model="editPostForm.title"
+                        required
+                        placeholder="Enter title">
+          </b-form-input>
+        </b-form-group>
+        <b-form-group id="form-body-edit-group"
+                      label="Body:"
+                      label-for="form-body-edit-input">
+          <b-form-input id="form-body-edit-input"
+                        type="text"
+                        v-model="editPostForm.body"
+                        required
+                        placeholder="Enter content">
+          </b-form-input>
+        </b-form-group>
+        <b-button type="submit" variant="primary">Update</b-button>
+        <b-button type="reset" variant="danger">Cancel</b-button>
+      </b-form>
+    </b-modal>
+
+  </div>
 </template>
 
 <script>
@@ -73,6 +114,11 @@ export default {
     return {
       posts: [],
       addPostFrom: {
+        title: '',
+        body: '',
+      },
+      editPostForm: {
+        id: '',
         title: '',
         body: '',
       },
@@ -109,9 +155,46 @@ export default {
           this.getPosts();
         });
     },
+    editPost(post) {
+      this.editPostForm = post;
+    },
+    removePost(postID) {
+      const path = `http://localhost:5000/posts/${postID}`;
+      axios.delete(path)
+        .then(() => {
+          this.getPosts();
+          this.message = 'Post removed!';
+          this.showMessage = true;
+        })
+        .catch((error) => {
+          // eslint-disable-next-line
+          console.log(error)
+          this.getPosts();
+        });
+    },
+    updatePost(payload, postID) {
+      const path = `http://localhost:5000/posts/${postID}`;
+      axios.put(path, payload)
+        .then(() => {
+          this.getPosts();
+          this.message = 'Post updated!';
+          this.showMessage = true;
+        })
+        .catch((error) => {
+          // eslint-disable-next-line
+          console.log(error)
+          this.getPosts();
+        });
+    },
+    onDeletePost(post) {
+      this.removePost(post.id);
+    },
     initForm() {
       this.addPostFrom.title = '';
       this.addPostFrom.body = '';
+      this.editPostForm.id = '';
+      this.editPostForm.title = '';
+      this.editPostForm.body = '';
     },
     onSubmit(evt) {
       evt.preventDefault();
@@ -123,15 +206,29 @@ export default {
       this.addPost(payload);
       this.initForm();
     },
+    onSubmitUpdate(evt) {
+      evt.preventDefault();
+      this.$refs.editPostModal.hide();
+      const payload = {
+        id: this.editPostForm.id,
+        title: this.editPostForm.title,
+        body: this.editPostForm.body,
+      };
+      this.updatePost(payload, this.editPostForm.id);
+    },
     onReset(evt) {
       evt.preventDefault();
       this.$refs.addPostModal.hide();
       this.initForm();
+    },
+    onResetUpdate(evt) {
+      evt.preventDefault();
+      this.$refs.editPostModal.hide();
+      this.getPosts();
     },
   },
   created() {
     this.getPosts();
   },
 };
-
 </script>
--- prototype/app.py
+++ prototype/app.py
@@ -46,6 +46,7 @@ def all_posts():
     if request.method == 'POST':
         post_data = request.get_json()
         POSTS.append({
+            'id': uuid.uuid4().hex,
             'title': post_data.get('title'),
             'body': post_data.get('body'),
             'author_id': post_data.get('author_id'),
@@ -60,13 +61,35 @@ def all_posts():
 
 @app.route('/posts/<post_id>', methods=['PUT', 'DELETE'])
 def single_post(post_id):
-    response_object = {'status': success}
+    response_object = {'status': 'success'}
+
+    if request.method == 'PUT':
+        indexnum = ""
+        post_data = request.get_json()
+        # indexの検索
+        for i, dic in enumerate(POSTS):
+            if dic["id"] == post_id:
+                index_num = i
+
+        # POSTSに修正
+        POSTS[index_num]["title"] = post_data.get('title')
+        POSTS[index_num]["body"] = post_data.get('body')
+
+        response_object['message'] = 'post updated!'
+
     if request.method == 'DELETE':
-        remove_post(book_id)
+        remove_post(post_id)
         response_object['message'] = 'post removed!'
 
     return jsonify(response_object)
 
+
+def remove_post(post_id):
+    for post in POSTS:
+        if post['id'] == post_id:
+            POSTS.remove(post)
+            return True
+    return false
 
 if __name__ == '__main__':
     app.run()
